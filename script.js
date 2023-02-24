a5237a4c09b6cd3949760b753344b58adb22aa33
Abhishek Manoj More <abhishek.mmore51@gmail.com>
2023-02-24 18:33:22 +0530

    basic api setup

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // API KEY
-const API_KEY= 'api_key=723dc50966fb83d2b5b758c4a9c895af';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL =  BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY;
+const API_KEY= 'api_key=723dc50966fb83d2b5b758c4a9c895af'; //To get this go to settings/api (API Key)
+const BASE_URL = 'https://api.themoviedb.org/3'; //To get this go to settings/api (Example API Request) 
+const API_URL =  BASE_URL + '/discover/movie?sort_by=popularity.desc&'+API_KEY; //api/discover Examples
+
