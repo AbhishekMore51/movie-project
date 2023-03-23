a2b1b16a98deabfd094506d53c54a5ffbf33faa8
AbhishekMore51 <abhishek.mmore51@gmail.com>
2023-03-23 20:42:30 +0530

    changes in cards and banner

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ const API_KIDS =
   BASE_URL +
   "/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&" +
   API_KEY; //Popular kids movie
+
+const banner_movie =
+  BASE_URL + "/discover/movie?sort_by=popularity.desc&" + API_KEY;
 // Images
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
@@ -19,6 +22,7 @@ const IMG_URL = "https://image.tmdb.org/t/p/w500";
 getmovies(API_URL);
 getPopularMovies(API_Popular);
 getPopularKids(API_KIDS);
+getBannerMovie(banner_movie);
 
 // Release Date
 function getmovies(url) {
@@ -27,6 +31,67 @@ function getmovies(url) {
     .then((data) => {
       // console.log(data, "release date");
     });
+}
+
+// Banner Movie
+function getBannerMovie(url) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data, "banner data");
+      let results = data.results;
+      showBannerMovies(results);
+    });
+}
+
+function showBannerMovies(results) {
+  let banner_section = document.createElement("div");
+  banner_section.innerHTML = `
+      <div class="banner-background" style="
+        background: linear-gradient(
+          to right,
+          rgb(0 0 0 / 0.5),
+          rgb(0 0 0 / 0)
+          ),
+          url('${IMG_URL + results[0].backdrop_path}')
+      ">
+        <div class="banner-desc-section">
+          <div class="img-container">
+              <img src="${
+                IMG_URL + results[0].poster_path
+              }" class="card-img-top" alt="...">
+          </div>
+
+          <div class="desc-container">
+            <div class="upper-title">
+                <h3>${results[0].original_title}</h3>
+              <div class="sub-upper-title d-flex">
+                <div>
+                  <i class="fas fa-language"></i>
+                  ${results[0].original_language}
+                </div>
+                <div>
+                  <i class="fas fa-thumbs-up"></i>
+                  ${results[0].vote_average}
+                </div>
+                <div>
+                  <i class="far fa-tomato"></i>
+                  ${results[0].vote_count}
+                </div>
+              </div>
+            </div>
+            <div class="lower-title mt-5">
+              ${results[0].overview}
+            </div>
+            <div class="title-btn mt-3">
+              <a href="#" class="btn btn-primary">More Detaiils</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      `;
+  document.getElementById("carousel-section").append(banner_section);
 }
 
 // Popular Movies
@@ -40,27 +105,33 @@ function getPopularMovies(url) {
       showPopularMovies(results);
     });
 }
-function showPopularMovies(results){
-
-  for(let i=0;i < 6 ; i++){
-    console.log(results,"aja")
+function showPopularMovies(results) {
+  for (let i = 0; i < 6; i++) {
+    // console.log(results,"aja")
     let movies = document.createElement("div");
-    movies.classList = "col-lg-4 col-md-4 col-sm-6 mt-5"
+    movies.classList = "col-lg-4 col-md-4 col-sm-6 mt-5";
     movies.innerHTML = `
-      <div class="card" >
-        <img src="${IMG_URL+results[i].poster_path}" class="card-img-top" alt="${results[i].title}">
+    <a class="card-links" href="#">
+      <div class="card card-detailing" >
+        <img src="${
+          IMG_URL + results[i].poster_path
+        }" class="card-img-top" alt="${results[i].title}">
         <div class="card-body card-body-hover">
           <h5 class="card-title title-text">${results[i].title}</h5>
-          <p class="card-text overview-text">${results[i].overview}</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+          <p class="card-para">
+            Average votes: 
+            <span style="margin-left: 10px">${results[0].vote_average} 
+              <i class="fas fa-star" style="color: #ffd700"></i>
+            </span>
+          </p>
         </div>
       </div>
-    `
+    </a>
+    `;
     document.getElementById("cards-popular").append(movies);
     // main.appendChild(movies);
   }
 }
-
 
 // Popular kids movies
 function getPopularKids(url) {
